7e4222aef4ce38732be7b114e0f5b22e910e6c8d
WEBCOMM\andy.wang <weat0212@gmail.com>
2021-09-07 11:24:17 +0800

    ngIf directive

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -7,15 +7,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  username = 'Andy Wang'
+  username = 'AndyWang'
   password = ''
 
+  errorMessage = 'Invalid Credentials'
+  invalidLogin = false
+  
   constructor() { }
 
   ngOnInit(): void {
   }
 
   handleLogin() {
-    console.log(this.username);
+    if(this.username === "AndyWang" && this.password === "password") {
+      this.invalidLogin = false
+    } else {
+      this.invalidLogin = true
+    }
   }
 }
