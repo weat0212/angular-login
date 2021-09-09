34d4998f11a066adf69bad8093ff1b67d91363c2
WEBCOMM\andy.wang <weat0212@gmail.com>
2021-09-09 18:38:53 +0800

    bootstrap styling

--- src/app/list-todos/list-todos.component.ts
+++ src/app/list-todos/list-todos.component.ts
@@ -1,5 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 
+export class Todo {
+  constructor(
+    public id: number,
+    public description: string,
+    public done: boolean,
+    public targetDate: Date
+  ) {
+
+  }
+}
+
 @Component({
   selector: 'app-list-todos',
   templateUrl: './list-todos.component.html',
@@ -7,11 +18,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListTodosComponent implements OnInit {
 
+  // Class
   todos = [
-    {id : 1, description : 'Finish Homework'},
-    {id : 2, description : 'Cook'},
-    {id : 3, description : 'Tv Series'},
+    new Todo(1, 'Finish Homework', false, new Date()),
+    new Todo(2, 'Cook', false, new Date()),
+    new Todo(3, 'Tv Series', false, new Date())
   ]
+
+  // List Component
+  // todos = [
+  //   {id : 1, description : 'Finish Homework'},
+  //   {id : 2, description : 'Cook'},
+  //   {id : 3, description : 'Tv Series'},
+  // ]
+
   // todo = {
   //   id : 1,
   //   description: 'Finish Homework'
