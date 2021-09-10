4f9954b23fd79b1e8389a1ca70c9c3b86ffd6557
WEBCOMM\andy.wang <weat0212@gmail.com>
2021-09-10 11:57:46 +0800

    Routing guard service

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,15 +4,16 @@ import { ErrorComponent } from './error/error.component';
 import { ListTodosComponent } from './list-todos/list-todos.component';
 import { LoginComponent } from './login/login.component';
 import { LogoutComponent } from './logout/logout.component';
+import { RouteGuardService } from './service/route-guard.service';
 import { WelcomeComponent } from './welcome/welcome.component';
 
 const routes: Routes = [
-  {path:"", component: LoginComponent},
-  {path:"login", component: LoginComponent},
-  {path:"welcome/:username", component: WelcomeComponent},
-  {path:"todos", component: ListTodosComponent},
-  {path:"logout", component: LogoutComponent},
-  {path:"**", component: ErrorComponent}
+  { path: "", component: LoginComponent },
+  { path: "login", component: LoginComponent },
+  { path: "welcome/:username", component: WelcomeComponent, canActivate:[RouteGuardService] },
+  { path: "todos", component: ListTodosComponent, canActivate:[RouteGuardService] },
+  { path: "logout", component: LogoutComponent, canActivate:[RouteGuardService] },
+  { path: "**", component: ErrorComponent }
 ];
 
 @NgModule({
