92e349746b9d5279588c2a0a57842351e985e67e
WEBCOMM\andy.wang <weat0212@gmail.com>
2021-09-11 13:47:04 +0800

    Rest API communication

--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { AppComponent } from '../app.component';
+import { HelloWorldBean, WelcomeDataService } from '../service/data/welcome-data.service';
 
 @Component({
   selector: 'app-welcome',
@@ -9,13 +10,29 @@ import { AppComponent } from '../app.component';
 })
 export class WelcomeComponent implements OnInit {
 
+  messageFromService:string = ''
   username = ''
 
   // ActivatedRoute
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute,
+              private service: WelcomeDataService) { }
 
   ngOnInit(): void {
     this.username = this.route.snapshot.params['username']
   }
 
+  getWelcomeMessage() {
+    // console.log(this.service.executeHelloWorldBeanService());
+    this.service.executeHelloWorldBeanService().subscribe(
+      response => this.handleSuccessfulResponse(response)
+    );
+
+    console.log('last line of getWelcomeMessage')
+  }
+
+  handleSuccessfulResponse(response:HelloWorldBean) {
+    this.messageFromService = response.message;
+    // console.log(response);
+    // console.log(response.message);
+  }
 }
